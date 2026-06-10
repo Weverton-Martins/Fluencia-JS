167ed605fa93fa8f92b7594a3eb3d332692f59f3
Weverton Martins <wevertonwhm18@outlook.com>
2026-06-10 20:58:57 -0300

    iniciando

---
+++ Etapa-01/app.js
@@ -0,0 +1,15 @@
+// comentario de uma linha
+/*comentario em bloco */
+let age = 26
+let currentYear = 2026
+
+console.log(age, currentYear)
+
+age = 27
+
+const points = 100
+
+console.log(points)
+
+// var score = 75
+// console.log(score)
