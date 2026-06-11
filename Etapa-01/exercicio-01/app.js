ef7ea0a51870664f33d31344e50cbe974b9085ac
Weverton Martins <wevertonwhm18@outlook.com>
2026-06-10 21:35:55 -0300

    .

---
+++ Etapa-01/exercicio-01/app.js
@@ -0,0 +1,14 @@
+// alert('JS is Awesome!')
+
+const myBirthYear = 2000
+const currentYear = 2026
+
+console.log(myBirthYear, currentYear)
+
+const myAge = currentYear - myBirthYear
+
+console.log(myAge)
+
+const ten = 10
+
+console.log(`Em ${currentYear + ten}, estarei com ${myAge + ten} anos.`)
