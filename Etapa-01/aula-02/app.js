402547e0b0b3c94a9dfdaaa39ffdc96c3ea622ff
Weverton Martins <wevertonwhm18@outlook.com>
2026-06-12 16:52:51 -0300

    .

--- Etapa-01/aula-02/app.js
+++ Etapa-01/aula-02/app.js
@@ -1,9 +1,8 @@
-/*
 //strings
 console.log('Hello, word!')
 
-const email = 'weverton@gmail.com'
-console.log(email)
+const email1 = 'weverton@gmail.com'
+console.log(email1)
 
 //concatenação strings
  const firstName = 'Weverton'
@@ -22,9 +21,8 @@ console.log(fullName.toUpperCase())
 const result = fullName.toLowerCase()
 console.log(result, fullName)
 
-const index = email.indexOf('@')
+const index = email1.indexOf('@')
 console.log(index)
-*/
 
 //métodos comuns de strings
 const email = 'jhonsnoll@gmail.com.br' 
@@ -37,3 +35,20 @@ console.log(slice)
 
 const emailReplace = email.replace('jhonsnoll', 'legolasbelmonth')
 console.log(emailReplace, email)
+
+const postTitle = 'É boalacha ou biscoito?'
+const postAuthor = 'Matheus Saad'
+const postComments = 15
+
+// usando concatenação
+// const postMensagem = 'O post "'+ postTitle + ' " do '+ postAuthor + 'está com '+ postComments + ' comentarios.'
+
+// usando template Strings
+const postMensagem = `O post "${postTitle}", do ${postAuthor}, tem ${postComments} comentarios`
+
+// criando templates HTML
+const html = `
+    <h2>${postTitle}</h2>
+    <p>${postAuthor}</p>
+    <span> Este post ${postComments}</span>
+`
