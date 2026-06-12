6123e18edb64a98d610d5ece0afd018b54031e55
Weverton Martins <wevertonwhm18@outlook.com>
2026-06-12 16:14:05 -0300

    continuando

---
+++ Etapa-01/aula-02/app.js
@@ -0,0 +1,39 @@
+/*
+//strings
+console.log('Hello, word!')
+
+const email = 'weverton@gmail.com'
+console.log(email)
+
+//concatenação strings
+ const firstName = 'Weverton'
+ const lastName = 'Snoll'
+ const fullName = firstName + ' ' + lastName
+ console.log(fullName)
+
+//acessando caracteres
+console.log(fullName[0])
+
+//comprimento de uma strings
+console.log(fullName.length)
+
+//métodos de strings
+console.log(fullName.toUpperCase())
+const result = fullName.toLowerCase()
+console.log(result, fullName)
+
+const index = email.indexOf('@')
+console.log(index)
+*/
+
+//métodos comuns de strings
+const email = 'jhonsnoll@gmail.com.br' 
+
+const lastIndexOfA = email.lastIndexOf('o')
+console.log(lastIndexOfA)
+
+const slice = email.slice(0, 9)
+console.log(slice)
+
+const emailReplace = email.replace('jhonsnoll', 'legolasbelmonth')
+console.log(emailReplace, email)
