b035fe15f21653f797e0b678dc5e101f7efa5175
Weverton Martins <wevertonwhm18@outlook.com>
2026-06-14 19:50:28 -0300

    .

---
+++ Etapa-01/aula-03/app.js
@@ -0,0 +1,14 @@
+// arrays
+let heroes = ['Batman', 'Catwoman', 'Iron Man']
+heroes[1] = 'Spider-Man'
+const age = [12,28,23,36]
+const randoArray = ['Parker', 17]
+
+// Métodos de arrays
+const joinHeroes = heroes.join(' - ')
+const indexOfHeroes = heroes.indexOf('Batman')
+const concatHeroes = heroes.concat(['Superman', 'Thor'])
+const pushHeroes = heroes.push('Hulk', 'Cyclops')
+const popHeroes = heroes.pop()
+
+console.log(heroes)
