ee1edbcdb0a1e23b0fc96e5293b3241fa3467979
Weverton Martins <wevertonwhm18@outlook.com>
2026-06-16 18:08:27 -0300

    etapa1-finalizada

---
+++ Etapa-01/aula-04/app.js
@@ -0,0 +1,39 @@
+// Booleans e comparações
+ console.log(true, false, 'True', 'False')
+
+
+// Métodos podem retornar booleans
+const email = 'brucewayne@gmail.com'
+// const includes = email.includes('@')
+
+const lista = ['Roger', 'Dio', 'Brand']
+const includes = lista.includes('Dio')
+
+
+// Operadores de comparação
+const age = 26
+const myName = 'Weverton'
+
+// console.log(age == 26)
+// console.log(age == 28)
+// console.log(age != 25)
+// console.log(age > 26)
+// console.log(age < 26)
+// console.log(age >= 26)
+// console.log(age <= 26)
+
+// console.log(myName == 'Weverton')
+// console.log(myName == 'weverton')
+// console.log(myName > 'weverton')
+
+// jeito mais seguro de comparar
+const agr = 26
+
+// console.log(age === 26)
+// console.log(age === '26')
+// console.log(age !== 26)S
+// console.log(age !== '26') 
+
+// COnversões de tipo
+
+ 
