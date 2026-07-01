157c21016d55107cbb27d6a69b5517da4b496093
Weverton Martins <wevertonwhm18@outlook.com>
2026-06-30 21:02:37 -0300

    Etapa_4

---
+++ etapa-04/aula-01/app.js
@@ -0,0 +1,23 @@
+// criando um objeto literal
+
+const user = {
+    name : 'Bruce',
+    age : 34,
+    email : 'brucewayne@gmail.com',
+    city : 'Gothan',
+    blogs : ['Cavaleiro das Trevas', 'Batman vs Superman']
+}
+
+// user.name = 'Clark'
+console.log(user.name)
+
+// user['age'] = 38
+
+console.log(user.age)
+
+// console.log(user['email'])
+
+// const key = 'city'
+// console.log(user[key])
+
+console.log(typeof user)
